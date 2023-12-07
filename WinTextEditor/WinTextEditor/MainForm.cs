d871409f0e28245ff53c8c161ab8f804663b12f4
LGVysotskij <leonid.vysotskij@klgtu.ru>
2023-12-07 17:31:43 +0200

    Создание программы WinTextEditor

---
+++ WinTextEditor/WinTextEditor/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 07.12.2023
+ * Time: 17:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinTextEditor
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
